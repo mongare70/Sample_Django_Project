06a96541fe852e499c4d1ff6f8244556be52ec2e
Hillary_Mongare <Hillary_Mongare@DESKTOP-8PFLNJB>
2019-10-22 09:15:38 +0300

    Sample Django Project

---
+++ calc/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {'name':'Hillary'}); 
+
+def add(request):
+    
+    value1 = int(request.POST['num1'])
+    value2 = int(request.POST['num2'])
+    res = value1 + value2
+    
+    return render(request, 'result.html', {'result': res})
